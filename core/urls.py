92971f2be580f334c2a4d22bbed14d07920a2206
Luciano Carniato <lucianocarniato@gmail.com>
2021-09-20 23:15:00 -0300

    Revert "Commit - Aplicação Dajango com Links de Apps Criados"
    This reverts commit c4d4567cb85fade777f089b7be1270175b921da7.

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls.conf import path
-
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home')
-]
--- instituicao/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('consultar/', views.consultarInstituicao, name='consultar-instituicao'),
-    path('excluir/', views.excluirInstituicao, name='excluir-instituicao'),
-    path('editar/', views.editarInstituicao, name='editar-instituicao'),
-    path('cadastrar/', views.cadastrarInstituicao, name='cadastrar-instituicao'),
-]
